6462ae9de24eb82bdc691e9a8f2f39c3986c57fc
wesley-lewis <lewis.wes.23@gmail.com>
2024-04-03 16:55:35 +0530

    current nvim config

---
+++ current-nvim/after/plugin/telescope.lua
@@ -0,0 +1,8 @@
+local builtin = require('telescope.builtin')
+vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
+vim.keymap.set('n', '<leader>fg', builtin.current_buffer_fuzzy_find, {})
+vim.keymap.set('n', '<C-p>', builtin.git_files, {})
+vim.keymap.set('n', '<leader><space>', builtin.buffers, {})
+vim.keymap.set('n', '<leader>ps', function()
+	builtin.grep_string({search = vim.fn.input("Grep > ") });
+end)
---
+++ current-nvim/after/plugin/toggleterm.lua
@@ -0,0 +1,7 @@
+local toggleterm = require("toggleterm")
+toggleterm.setup({
+	direction = "horizontal",
+	size = 80,
+	open_mapping = [[<M-j>]]
+})
+
